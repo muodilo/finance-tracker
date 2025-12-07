f2b849a683b7fc2884ee03ecbf95946f9bdff72f
muodilo <odilomurindahabi@gmail.com>
2025-12-07 20:05:20 +0200

    chore: add configuration of firebase

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7w7PSDFRHu75vRdlxPThX4upRtg0CYiw",
+  authDomain: "finance-tracker-80982.firebaseapp.com",
+  projectId: "finance-tracker-80982",
+  storageBucket: "finance-tracker-80982.firebasestorage.app",
+  messagingSenderId: "119082331160",
+  appId: "1:119082331160:web:0d6a25d8b619ddaabfa76d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
